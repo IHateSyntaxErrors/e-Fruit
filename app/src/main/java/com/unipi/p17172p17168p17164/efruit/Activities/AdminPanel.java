35a1d11b6601e3df76f4b9546ba30b6fa987c955
Andreas Theodoridis <antreastheodorides16@gmail.com>
2021-03-02 22:21:52 +0200

    Admin panel

---
+++ app/src/main/java/com/unipi/p17172p17168p17164/efruit/Activities/AdminPanel.java
@@ -0,0 +1,161 @@
+package com.unipi.p17172p17168p17164.efruit.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.geofire.GeoFireUtils;
+import com.firebase.geofire.GeoLocation;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingEvent;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.unipi.p17172p17168p17164.efruit.R;
+import com.unipi.p17172p17168p17164.efruit.Utils.Toolbox;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AdminPanel extends AppCompatActivity {
+    private FirebaseFirestore db;
+    public LinearLayoutManager linearLayoutManager;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private Toolbox toolbox;
+    private LocationManager locationManager;
+    private Context context;
+    public String provider;
+    RecyclerView recyclerList;
+    final String TAG = "[Coords List]";
+    final String TAG1 = "[users list]";
+    final String TAG2 = "[shops list]";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_panel);
+        recyclerList.setLayoutManager(linearLayoutManager);
+        recyclerList.setHasFixedSize(true);
+
+
+        db = FirebaseFirestore.getInstance();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        toolbox = new Toolbox();
+        db.collection("users").whereEqualTo("tokenId", true).whereEqualTo("is_admin",false).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for (QueryDocumentSnapshot document : task.getResult()){
+                        Log.d(TAG1, document.getId() + " => " + document.getData());
+                    }
+                    db.collection("users").whereEqualTo("coords",true).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @RequiresApi(api = Build.VERSION_CODES.Q)
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Log.d(TAG, document.getId() + " => " + document.getData());
+                                }
+                                getLocation();
+                            }
+                        }
+                    });
+                }else{
+                    Log.d(TAG1, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+        db.collection("shops").whereEqualTo("coords",true).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG2, document.getId() + " => " + document.getData());
+                    }
+            }else{
+                    Log.d(TAG2, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+    }
+
+
+    @SuppressLint("MissingPermission")
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private void getLocation() {
+        locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+        boolean enabled = locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        // check if enabled and if not send user to the GSP settings
+        // Better solution would be to display a dialog and suggesting to
+        // go to the settings
+        if (enabled) {
+            // Define the criteria how to select the location provider -> use
+            // default
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+            criteria.setPowerRequirement(Criteria.POWER_LOW);
+            criteria.setAltitudeRequired(false);
+            criteria.setBearingRequired(false);
+            criteria.setSpeedRequired(false);
+            criteria.setCostAllowed(true);
+            criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+            criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+            provider = locationManager.getBestProvider(criteria, false);
+            Location location = locationManager.getLastKnownLocation(provider);
+            if (location != null) {
+                onLocationChanged(location);
+            }
+        } else {
+            AlertDialog alertDialog = toolbox.buildAlertMessageNoGps(context);
+            alertDialog.show();
+        }
+    }
+    private void onLocationChanged(Location location) {
+        double latUser = location.getLatitude();
+        double lngUser = location.getLongitude();
+        String hash = GeoFireUtils.getGeoHashForLocation(new GeoLocation(latUser, lngUser));
+        Map<String, Object> updates = new HashMap<>();
+        updates.put("location", new GeoPoint(latUser, lngUser));
+        updates.put("geohash", hash);
+        DocumentReference locationRef = db.collection("users").document(firebaseUser.getUid());
+        locationRef.update(updates)
+                .addOnCompleteListener(task -> {
+
+                });
+    }
+}
+
